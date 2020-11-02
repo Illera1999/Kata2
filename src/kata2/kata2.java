ac074cd4eb3b9319d160cc2b2295c5d9576716e3
josei <josei@DESKTOP-48TQE4N>
2020-11-02 13:10:37 +0000

    Kata2: versión->2: operador condicional ternario

--- src/kata2/kata2.java
+++ src/kata2/kata2.java
@@ -11,13 +11,8 @@ public class kata2 {
         int [] data = {1,2,3,1,2,1,2,3,1,2,3,2,2,1,2};
         histograma res = new histograma ();
            
-        for (int i = 0; i < data.length; i++) {
-           int j = data[i];
-           if (res.contiene(data[i])){
-               res.put(data[i], res.get(data[i])+1);
-           }else {
-               res.put(data[i],1);
-           }
+        for (int key : data) {
+            res.put(key,res.contiene(key)?res.get(key)+1:1);
         }
         
         System.out.println(res);
